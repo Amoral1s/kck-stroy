c385c34ac27459bc2ec47831d09351f94a8ea5f2
Дмитрий Филиппов <Amoralis.19@gmail.com>
2019-10-14 14:27:22 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,16 +1,51 @@
 $(document).ready(function () {
-  
-  $('.feedback-wrap').slick({
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    infinite: true,
-      arrows: true,
-      dots: true,
-      autoplay: false,
-      autoplayspeed: 3000,
-      speed: 500
-    });
 
+  $('#menu').click(function () { 
+    $(this).toggleClass('menu-active');
+    $('.header-nav ul.container').toggleClass('nav-menu-active');
+    $('.header-nav ul.container').slideToggle(600);
+   });
+
+  $('.header-nav ul li:has(ul)').addClass('has-children');
+
+  $('li.has-children').click(function() {
+    $(this).children('ul').slideDown(500);
+  });
+
+  $('li.has-children').mouseenter(function() {
+    $(this).children('ul').slideDown(500);
+  });
+  
+  $('.has-children').mouseleave(function() {
+    $(this).children('ul').slideUp(500);
+  });
+  
+  if($(window).width() < 768) {
+
+    $('.feedback-wrap').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+        arrows: true,
+        dots: true,
+        autoplay: false,
+        autoplayspeed: 3000,
+        speed: 500
+      });
+    } else {
+
+      $('.feedback-wrap').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+          arrows: true,
+          dots: true,
+          autoplay: false,
+          autoplayspeed: 3000,
+          speed: 500
+        });
+
+    }
 
 
 
